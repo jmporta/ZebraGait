86ee03cf7093d0c9edbc57920264294cc42a0ba8
avalls <avalls@iri.upc.edu>
2018-10-19 09:33:58 +0200

    progress bar/threading

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-# Python libs
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
+from threading import Thread
 
-# Local libs
 from swimTunnel import swimTunnel
 from treatData import treatData
 
-# FUNCTIONS
+### FUNCTIONS ###
+
+# Main functions
 
 def runProcess():
+    # The selected video will be processed and the resultant data will be treated.
+    # It saves the skeletons in a "./data/" and a video in "./export" using swimTunnel function.
+    # It saves a cvs file with the treated data in a "./export" using treatData function.
     try:
         filePath = txtPath.get()
         expID = txtVidId.get()
@@ -24,14 +28,48 @@ def runProcess():
     else:
         tk.messagebox.showinfo("Info", "The video has been processed. Check the results in './export' folder.")
 
-
 def showResults():
+     # It shows the data in the "./export" folder.  TODO: save the data on the desired folder.
     pass
 
-# MAIN WIDGETS
+# Click functions
+
+def clickPath():
+    filePath = tk.filedialog.askopenfilename(initialdir="/home/", title="Select file", filetypes=(("avi files", "*.avi"), ("all files", "*.*")))
+    txtPath.delete(0, tk.END)
+    txtPath.insert(0, filePath)
+
+def clickRun():
+    run_thread('run', runProcess)
+    
+def clickShow():
+    run_thread('show',showResults)
+
+# Threading functions to run the progressbar
+
+def run_thread(name, func):
+    Thread(target=run_function, args=(name, func)).start()
+
+def run_function(name, func):
+    # Disable all buttons
+    for btn in buttons:
+        btn['state'] = 'disabled'
+
+    progressbar.start(interval=10)
+    func()
+    progressbar.stop()
+
+    # Enable all buttons
+    for btn in buttons:
+        btn['state'] = 'normal'
+
+### GUI ###
+
+# WINDOW PROPERTIES
 
 # Main window
 mainWin = tk.Tk()
+
 mainWin.title("Swim Tunnel")
 mainWin.resizable(width=False, height=False)
 
@@ -39,13 +77,11 @@ mainWin.resizable(width=False, height=False)
 img = tk.PhotoImage(file='gar-fish.png')
 mainWin.tk.call('wm', 'iconphoto', mainWin._w, img)
 
+# FIELDS FRAME 
+
 # Main frame
 mainFrame = tk.Frame(mainWin, padx=5, pady=5)
 mainFrame.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-
-# Buttons frame
-btnFrame = tk.Frame(mainWin, padx=10, pady=10)
-btnFrame.grid(column=0, row=1, sticky=(tk.N, tk.E, tk.S), columnspan=4)
 
 # Video open path
 lblPath = tk.Label(mainFrame, text="Video path:")
@@ -54,11 +90,6 @@ lblPath.grid(column=0,row=0, sticky=tk.W, padx=10,pady=10)
 defaultPath = tk.StringVar(mainFrame, value='./video/water_tunnel.avi')
 txtPath = tk.Entry(mainFrame, width=35,textvariable=defaultPath)
 txtPath.grid(column=1, row=0, sticky=(tk.W, tk.E), padx=5, pady=5, columnspan=3)
-
-def clickPath():
-    filePath = tk.filedialog.askopenfilename(initialdir="/home/",title="Select file", filetypes=(("avi files", "*.avi"), ("all files", "*.*")))
-    txtPath.delete(0, tk.END)
-    txtPath.insert(0, filePath)
 
 btnPath = tk.Button(mainFrame, text="Open", command=clickPath)
 btnPath.grid(column=4, row=0, sticky=tk.W, padx=5, pady=5)
@@ -79,30 +110,29 @@ defaultFps = tk.StringVar(mainFrame, value='1000')
 txtVidFps = tk.Entry(mainFrame,width=5,textvariable=defaultFps)
 txtVidFps.grid(column=3, row=1, sticky=(tk.W, tk.E), padx=5, pady=5)
 
+# RUN FRAME
+
+# Buttons frame
+btnFrame = tk.Frame(mainWin, padx=10, pady=10)
+btnFrame.grid(column=0, row=1, sticky=(tk.N, tk.E, tk.S))
+
 # Progress bar
 progressbar = ttk.Progressbar(btnFrame, mode="indeterminate")
-progressbar.grid(column=0,row=0)
-progressbar.start()
+progressbar.grid(column=0, row=0, sticky=tk.W, padx=5,pady=5)
 
-# BUTTONS
+# Spacer
+spacer = tk.Label(btnFrame, text=" ")
+spacer.grid(column=1, row=0, sticky=tk.W, padx=110, pady=5)
 
 # Run button
-# The selected video will be processed and the resultant data will be treated. 
-# It saves the skeletons in a "./data/" and a video in "./export" using swimTunnel function. 
-# It saves a cvs file with the treated data in a "./export" using treatData function.
-def clickRun():
-    runProcess()
-
-btnVid = tk.Button(btnFrame, text="Run", width=5, command=clickRun)
-btnVid.grid(column=1, row=0, sticky=tk.E, padx=5, pady=5)
+btnRun = tk.Button(btnFrame, text="Run", width=5, command=clickRun)
+btnRun.grid(column=3, row=0, sticky=tk.E, padx=5, pady=5)
 
 # Show button.
-# It shows the data in the "./export" folder.  TODO: save the data on the desired folder.
-def clickShow():
-    showResults()
+btnShow = tk.Button(btnFrame, text="Show", width=5, command=clickShow)
+btnShow.grid(column=2, row=0, sticky=tk.E, padx=5, pady=5)
 
-btnVid = tk.Button(btnFrame, text="Show", width=5, command=clickShow)
-btnVid.grid(column=0, row=0, sticky=tk.E, padx=5, pady=5)
+buttons = [btnRun, btnShow]
 
 
 mainWin.mainloop()
--- swimTunnel.py
+++ swimTunnel.py
@@ -3,25 +3,43 @@ import numpy as np
 
 
 def swimTunnel(filePath,expID):
-    # Get the video
+
+    # Open the video in a VideoCapture object
     vid = cv.VideoCapture(filePath)
+
+    # Check if the video object has opened the reference
+    if (not vid.isOpened()):
+        raise Exception("Could not open the video reference: " + filePath)
+
+    # Define main objects
+    totalFrames = 0
+    failFrames = 0
+
+    # Read the first frame for second video walk
     _, frame = vid.read()
 
     # Select ROI
-    r = cv.selectROI(frame)
+    crop = cv.selectROI(frame)
+    cv.destroyAllWindows()
 
     while(frame is not None):
 
-        # Crop image
-        frame = frame[r[1]:(r[1]+r[3]), r[0]:(r[0]+r[2])]
+        totalFrames += 1
+
+        # Step1 -- Crop the region of interest
+        frame = frame[crop[1]:(crop[1]+crop[3]), crop[0]:(crop[0]+crop[2])]
         originalFrame = frame
 
-        frame = preprocess(frame)
+        # Step2 - - PreProcess the image
+        frame = preprocess(frame, True, True)
+
+        # Step3.2 -- Fish contour and skeleton detection
         fishContours = getFishContours(frame)
         fishSkeleton = getFishSkeleton(frame)
 
+        
         x,y,w,h = cv.boundingRect(fishContours)
-        cv.polylines(originalFrame, fishContours, True,(0, 255, 0), 2)
+        cv.polylines(originalFrame, fishContours, True,(0, 255, 0), 1)
         cv.polylines(originalFrame, fishSkeleton, True, (0, 0, 255), 1)
         cv.rectangle(originalFrame, (x,y),(w+x,y+h), (255, 255, 255), 1, 8, 0)
         
@@ -33,7 +51,7 @@ def swimTunnel(filePath,expID):
     vid.release()
     cv.destroyAllWindows()
 
-def preprocess(frame):
+def preprocess(frame, blur, threshold):
 
     # Force B&W
     frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -43,12 +61,18 @@ def preprocess(frame):
     table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
     frame = cv.LUT(frame, table)
 
-    # Clean Noise
-    frame = cv.GaussianBlur(frame, (3, 3), 0, 0)
-    frame = cv.medianBlur(frame, 9)
+    if (blur):
+        # Clean Noise
+        frame = cv.GaussianBlur(frame, (3, 3), 0, 0)
+        frame = cv.medianBlur(frame, 9)
 
-    # Threshold by color to a binary image
-    frame = cv.inRange(frame, 0, 110)
+    if (threshold):
+        # Threshold by color to a binary image
+        frame = cv.inRange(frame, 0, 110)
+
+    # Debug Only: Show preprocess image
+    cv.imshow("PreProcess",frame)
+    cv.waitKey(1)
 
     return frame
 
@@ -58,16 +82,29 @@ def getFishContours(frame):
     frame = cv.Canny(frame, 100, 200)
 
     # Find and draw Contours
-    frame, contours, _ = cv.findContours(frame, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    frame, contours, _ = cv.findContours(frame, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
-    # Delete unnecessary blobs
+    # Delete unnecessary blobs TODO: No absolute values!
     iFishContour = 0
     for i in range(np.size(contours)):
+
         area = cv.contourArea(contours[i], False)
 
         if (area > 5500 and area < 6500):
             iFishContour = i
     
+    # Debug Only: Draw all contours
+    drawing = np.zeros((np.size(frame, 0), np.size(frame, 1)), np.uint8)
+    cv.drawContours(drawing, contours, iFishContour, (255, 255, 255), 2)
+    cv.imshow("Fish Contours", drawing)
+    cv.waitKey(1)
+
+    # # Debug Only: Draw all contours
+    # drawing2 = np.zeros((np.size(frame, 0), np.size(frame, 1)), np.uint8)
+    # cv.drawContours(drawing2,contours,-1,(0,0,255),2)#-1 to print all contours
+    # cv.imshow("All Contours",drawing2)
+    # cv.waitKey(1)
+
     return contours[iFishContour]
 
 def getFishSkeleton(frame):
