c338fa9db89f208651ebb98b6a5121947cbaa715
avalls <avalls@iri.upc.edu>
2019-02-07 14:30:56 +0100

    test path

--- swimTunnel.py
+++ swimTunnel.py
@@ -393,7 +393,7 @@ if (__name__ == "__main__"):
     )
 
     fps = 1000
-    videoPath = "./test/fishTest.avi"
+    videoPath = "./test/testVideo.avi"
     expID = "testID"
     exportPath = "./export/"
 
