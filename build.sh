4b31442cb6b74ff4017367c08fc21ecc8a387cd6
avalls <avalls@iri.upc.edu>
2019-02-28 16:06:13 +0100

    fixs

--- build.sh
+++ build.sh
@@ -2,3 +2,9 @@ pyinstaller --noconfirm --log-level=INFO \
     --onefile --noconsole\
     --add-data="README.md:." \
     zebraGait_Qt.py
+
+rm -rf build
+rm -rf logs
+rm -rf export
+rm -rf __pycache__
+rm zebraGait_Qt.spec
