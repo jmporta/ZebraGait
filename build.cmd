c35fe9cdd9293935cbd20b66dda59556492dede1
Arnau Valls Brusco <avalls@iri.upc.edu>
2019-03-01 18:00:40 +0100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,33 +1,35 @@
 @echo OFF
 
 :: lib = <PATH_TO_PYtHON>\Lib\site-packages\cv2\opencv_ffmpeg<VERSION>.dll
-set lib=%2
 set arg=%1
+set lib=%2
+
+:: main
+IF NOT "%arg%"=="clean" (
+    call :build_onefile
+) ELSE (
+    call :clean
+)
+
+EXIT /B 0
 
 :: Pyinstaller routine with options
 :build_onefile
 pyinstaller --noconfirm --log-level=INFO ^
     --onefile --noconsole ^
     --add-data="README.md;." ^
-    --add-binary "lib;." ^
+    --add-binary "%lib%;." ^
     --icon=.\models\icons\gar-fish.ico ^
-    arg
+    %arg%
 EXIT /B 0
 
 :: Clean all the build/execution extra files
 :clean
-    del build
-    del logs
-    del export
-    del __pycache__
-    del models\__pycache__
+    del -r build
+    del -r dist
+    del -r logs
+    del -r export
+    del -r __pycache__
+    del -r  models\__pycache__
     del *.spec
 EXIT /B 0
-
-:: main
-IF %1!="clean"
-    call build_onefile arg, lib
-    call clean
-ELSE
-    call clean
-    del dist
